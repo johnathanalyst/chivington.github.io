482a79828011d8c2d4d48e6a257a7aa33da257ba
Johnathan Chivington <chivingtoninc@gmail.com>
2018-11-16 00:38:59 -0800

    Added router and test routes

--- app.js
+++ app.js
@@ -7,88 +7,77 @@
 
 /* ------------------------------------- Libs ------------------------------------- */
 // React - for creating elements and diffing/maintaining vdom tree
-function React() {
-  return {
-    createElement: function(elem, attrs, children) {
-      const element = document.createElement(elem);
+React = {
+  createElement: function(elem, attrs, children) {
+    const element = document.createElement(elem);
 
-      if (attrs) Object.keys(attrs).forEach(k => element.setAttribute(k, attrs[k]));
+    if (attrs) Object.keys(attrs).forEach(k => element.setAttribute(k, attrs[k]));
 
-      if (children.length >= 1) children.forEach(child => element.appendChild((typeof child == "string")
-        ? document.createTextNode(child)
-        : (child.elem) ? child.elem(child.props, child.dispatch, child.children) : child
-      ));
+    if (children.length >= 1) children.forEach(child => element.appendChild((typeof child == "string")
+      ? document.createTextNode(child)
+      : (child.elem) ? child.elem(child.props, child.dispatch, child.children) : child
+    ));
 
-      return element;
-    }
+    return element;
   }
 }
 
 // ReactDOM - for rendering/updating dom based on vdom tree
-function ReactDOM() {
-  return {
-    render: function(component, root) {
-      while (root.children[0]) root.removeChild(root.children[0]);
-      root.appendChild(component.elem(component.props, component.dispatch, component.children));
-    }
+ReactDOM = {
+  render: function(component, root) {
+    while (root.children[0]) root.removeChild(root.children[0]);
+    root.appendChild(component.elem(component.props, component.dispatch, component.children));
   }
 }
 
 // Redux - for maintaining application state
-function Redux() {
-  return {
-    createStore: function(stateReducer, middlewares) {
-      var state = {}, listeners = [];
+Redux = {
+  createStore: function(stateReducer, middlewares) {
+    var state = {}, listeners = [];
 
-      function getState() {
-        return state;
+    function getState() {
+      return state;
+    }
+
+    function dispatch(action) {
+      if (middlewares.logActions) middlewares.logActions("before", state, action);
+      state = stateReducer(state, action);
+      if (middlewares.logActions) middlewares.logActions("after", state, action);
+      listeners.forEach(listener => listener.func(...listener.params));
+    }
+
+    function subscribe(listener) {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener);
       }
+    }
 
-      function dispatch(action) {
-        if (middlewares.logActions) middlewares.logActions("before", state, action);
-        state = stateReducer(state, action);
-        if (middlewares.logActions) middlewares.logActions("after", state, action);
-        listeners.forEach(listener => listener.func(...listener.params));
-      }
+    dispatch({type: "@@INIT"});
 
-      function subscribe(listener) {
-        listeners.push(listener);
-        return () => {
-          listeners = listeners.filter(l => l !== listener);
+    return { getState, dispatch, subscribe };
+  },
+  combineReducers: function(reducers) {
+    return (state, action) => {
+      return Object.keys(reducers).reduce((combined, k) => {
+        combined[k] = reducers[k](state[k], action);
+        return combined;
+      }, {});
+    }
+  },
+  storeMiddlewares: {
+    logActions: function(stage, state, action) {
+      if  (action.type != "@@INIT") {
+        if (stage == "before") {
+          console.log("\n%cCurrent State: ", "font-weight: bold; color: #0b0;", state);
+          console.log(`Action Dispatched: %c"${action.type}"`, "color: #e00;");
         }
-      }
-
-      dispatch({type: "@@INIT"});
-
-      return { getState, dispatch, subscribe };
-    },
-    combineReducers: function(reducers) {
-      return (state, action) => {
-        return Object.keys(reducers).reduce((combined, k) => {
-          combined[k] = reducers[k](state[k], action);
-          return combined;
-        }, {});
-      }
-    },
-    storeMiddlewares: {
-      logActions: function(stage, state, action) {
-        if  (action.type != "@@INIT") {
-          if (stage == "before") {
-            console.log("\n%cCurrent State: ", "font-weight: bold; color: #0b0;", state);
-            console.log(`Action Dispatched: %c"${action.type}"`, "color: #e00;");
-          }
-          if (stage == "after")
-            console.log("%cUpdated State: ", "font-weight: bold; color: #0b0;", state);
-        }
+        if (stage == "after")
+          console.log("%cUpdated State: ", "font-weight: bold; color: #0b0;", state);
       }
     }
   }
 }
-
-// -- Initialize/import Libs
-React = React();
-ReactDOM = ReactDOM();
-Redux = Redux();
 
 
 /* -------------------------------- App Components -------------------------------- */
@@ -108,6 +97,7 @@ Components = {
     return React.createElement("div", {style: styles.shell}, [
       { elem: Components.Header, props: { store }, dispatch: dispatch, children: [] },
       { elem: Components.Menu, props: { store }, dispatch: dispatch, children: [] },
+      { elem: Components.Router, props: { store }, dispatch: dispatch, children: [] },
       ...children
     ]);
   },
@@ -131,16 +121,21 @@ Components = {
     const title = React.createElement("h1", {style: styles.title}, ["c.ai"]);
     title.addEventListener("click", function() {
       dispatch({type: "CLOSE_MENU"});
-      dispatch({type: "NAV_TO", view: "HOME"})
+      dispatch({type: "NAV_TO", payload: "HOME"})
     });
 
-    return React.createElement("div", {style: styles.header}, [icon, title]);
+    const header = React.createElement("div", {style: styles.header}, [icon, title]);
+    // header.addEventListener("click", function(){
+    //   dispatch({type: "CLOSE_MENU"});
+    // });
+
+    return header;
   },
   // Menu Component - layered/collapsible full-route menu.
-  Menu: function (props, dispatch, children) {
+  Menu: function(props, dispatch, children) {
     const styles = {
       menuOpen: `
-        position: absolute; top: 2.8em; left: 0; bottom: 0; width: 7em; padding: 0.25em 1em 0 0;
+        position: absolute; top: 2.8em; left: 0; bottom: 0; width: 7em; padding: 0.25em 1em 0 0; z-index: 10;
         display: flex; flex-direction: column; justify-content: flex-start; align-items: stretch;
         background-color: rgba(20, 20, 60, 1); border-right: 1px solid #000; animation: menuOpen 0.15s 1;
       `,
@@ -157,19 +152,19 @@ Components = {
     const home = React.createElement("a", {style: styles.link}, ["Home"]);
     home.addEventListener("click", function(){
       dispatch({type: "CLOSE_MENU"});
-      dispatch({type: "NAV_TO", view: "HOME"});
+      dispatch({type: "NAV_TO", payload: "HOME"});
     });
 
     const about = React.createElement("a", {style: styles.link}, ["Me"]);
     about.addEventListener("click", function() {
       dispatch({type: "CLOSE_MENU"});
-      dispatch({type: "NAV_TO", view: "ABOUT"});
+      dispatch({type: "NAV_TO", payload: "ABOUT"});
     });
 
     const projects = React.createElement("a", {style: styles.link}, ["Projects"]);
     projects.addEventListener("click", function () {
       dispatch({type: "CLOSE_MENU"});
-      dispatch({type: "NAV_TO", view: "PROJECTS"});
+      dispatch({type: "NAV_TO", payload: "PROJECTS"});
     });
 
 
@@ -177,7 +172,24 @@ Components = {
   },
   // Router Component - maintains view routes. (viewing, tabs, minimized...)
   Router: function(props, dispatch, children) {
-    // Router code here
+    const styles = {
+      router: `
+        position: absolute; top: 2.75em; left: 0; bottom: 0; right: 0;
+        display: flex; flex-direction: column; justify-content: center; align-items: center;
+        background-color: #07e;
+      `
+    }
+
+    const view = props.store.getState().viewState;
+    const stl = `color: #fff; font-family: sans-serif;`;
+    const msg = React.createElement("a", {style: stl, href:"https://github.com/chivingtoninc"}, ["https://github.com/chivingtoninc"]);
+
+    const router = React.createElement("div", {style: styles.router}, [view, msg]);
+    router.addEventListener("click", function(){
+      dispatch({type: "CLOSE_MENU"});
+    });
+
+    return router;
   }
 }
 
