53bc1530714518aad8545e7d8b9088599b883914
Johnathan Chivington <chivingtoninc@gmail.com>
2018-11-30 21:39:04 -0800

    Updated Guide module

--- app.js
+++ app.js
@@ -621,7 +621,7 @@ const Components = {
       `,
       box: (x,y,h,w,r) => `
         position: absolute; top: ${y}; left: ${x}; height: ${h}; width: ${w}; z-index: 1000; background-color: rgba(0,0,0,0);
-        border-radius: ${r}; box-shadow: 0 0 0 1000em rgba(0,0,0,0.9);
+        border-radius: ${r}; -webkit-box-shadow: 0 0 0 1000em rgba(0,0,0,0.9);
       `,
       hidden: `display: none;`,
       msg: (x,y,h,w,r) => `
@@ -788,8 +788,6 @@ const Components = {
          box: {bx:"0.655em", by:"0.5em", bh:"3em", bw:"3em", br:"100%"},
          msg: {position: {mx:"4em", my:"0.7em", mh:"1em", mw:"3em"}, txt: "Tap the brain for more..."},
          btn: {position: {btx:"7em", bty:"2em", bth:"1.25em", btw:"3.5em", btr: "7px"}, txt: "Got it."}
-         // msg: {position: {mx:"0.86em", my:"2.7em", mh:"1em", mw:"3em"}, txt: "Touch the brain..."},
-         // btn: {position: {btx:"0.25em", bty:"4em", bth:0, btw:0}, txt: "Got it!"}
        }});
      }
 
