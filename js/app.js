f483a6df3aec51cc9151d0d909f13a3342124edb
chivingtoninc <chivingtoninc@gmail.com>
2019-07-25 22:12:10 -0700

    README.md refactor - project summary; version details.

--- js/app.js
+++ js/app.js
@@ -758,8 +758,8 @@ const Views = {
         background: linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.7) ), url('${wp_yolo}');
         background-position: center; background-size: cover; background-repeat: no-repeat; text-align: center;
       `,
-      work: `margin: 1em 0.5em; color: ${light}; font-size: 1.5em; font-weight: 900;`,
-      school: `margin: 0; color: ${light}; font-size: 1.25em; font-weight: 700;`,
+      work: `margin: 1em 0; color: ${light}; font-size: 1.25em; font-weight: 900;`,
+      school: `margin: 0; color: ${light}; font-size: 1.15em; font-weight: 700;`,
       intern: `margin: 1em; color: ${light}; font-size: 1em; font-weight: 500;`
     };
 
